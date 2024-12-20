5f03c4227a8e47045de9855b63e49a356184823e
Prasannakoduri <120080385+Prasannakodui@users.noreply.github.com>
2024-12-20 18:14:42 +0530

    international page

---
+++ src/pages/international.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const international = () => {
+  return (
+    <>
+    <div className='main'>
+        <div className='inter_banr'>
+        <img src={"/assets/img/international-3.jpg"}   alt="property" />
+        <div className='container'>
+
+            <div className='int_iner_sec'>
+            <div className='text-center inner_text'>
+            <h4>Launching Soon..</h4>
+            <h3>unimakler international</h3>
+            </div>
+
+            <Carousel>
+
+                <Carousel.Item>
+                    <h2> "Your Passport to Premier Properties<br></br> Worldwide." </h2>
+                </Carousel.Item>
+                <Carousel.Item>
+                <h2>  "Unlock International Opportunities with<br></br> unimakler Homes."  </h2>
+                </Carousel.Item>
+
+            </Carousel>
+              </div>
+        </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default international
